e87a80c95a7d843ba8950b5f88dcc219bb4439ac
ibrah-55 <karikuibrahim55@gmail.com>
2020-09-29 20:11:53 +0300

    added taking turns

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Square extends React.Component {
-	render() {
-		return (
-			<button className="square" onClick={() => this.props.onClick()}>
-				{this.props.value}
-			</button>
-		);
-	}
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
@@ -17,6 +15,7 @@ class Board extends React.Component {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
 	renderSquare(i) {
@@ -25,8 +24,11 @@ class Board extends React.Component {
 
 	handleClick(i) {
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({ squares: squares });
+		squares[i] = this.state.xIsNext ? 'X' : '0';
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
 	}
 
 	renderSquare(i) {
@@ -34,8 +36,7 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = 'Next player: X';
-
+		const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : '0');
 		return (
 			<div>
 				<div className="status">{status}</div>
